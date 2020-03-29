6a0a1c65955afd162279d854016f7dd1e86621ee
Piotr Pawlak <piotr.pawlak@pl.ibm.com>
2020-03-29 14:02:01 +0200

    [#8] Logger file.

--- src/golang-book-api/utils/logger.go
+++ src/golang-book-api/utils/logger.go
@@ -7,17 +7,24 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-// ASCII colors numbers
-const (
-	red    = 31
-	grey   = 37
-	blue   = 36
-	yellow = 33
-)
-
-// loggerTextFormatter object for formatting text in chaincode logs
 type loggerTextFormatter struct {
 	logrus.TextFormatter
+}
+
+// Format logger output
+func (f *loggerTextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(fmt.Sprintf(" %s \x1b[%dm%s\x1b[0m %s\n", entry.Time.Format(f.TimestampFormat), setLogLevelColor(entry), "["+strings.ToUpper(entry.Level.String())+"]", entry.Message)), nil
+}
+
+func setLogLevelColor(entry *logrus.Entry) int {
+	switch entry.Level {
+	case logrus.DebugLevel, logrus.TraceLevel:
+		return 36
+	case logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel:
+		return 31
+	default:
+		return 37
+	}
 }
 
 // NewLogger function returns Logger object
@@ -33,32 +40,4 @@ func NewLogger(logLevel logrus.Level) *logrus.Logger {
 	}
 	logger.Level = logLevel
 	return logger
-}
-
-// Format logger output
-func (f *loggerTextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	logLevel := "[" + strings.ToUpper(entry.Level.String()) + "]"
-	return []byte(
-		fmt.Sprintf(" %s \x1b[%dm%s\x1b[0m %s\n",
-			entry.Time.Format(f.TimestampFormat),
-			setLogLevelColor(entry),
-			logLevel,
-			entry.Message,
-		),
-	), nil
-}
-
-func setLogLevelColor(entry *logrus.Entry) int {
-	var levelColor int
-	switch entry.Level {
-	case logrus.DebugLevel, logrus.TraceLevel:
-		levelColor = blue
-	case logrus.WarnLevel:
-		levelColor = yellow
-	case logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel:
-		levelColor = red
-	default:
-		levelColor = grey
-	}
-	return levelColor
 }
